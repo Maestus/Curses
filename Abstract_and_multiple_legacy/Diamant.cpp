7a7c3233cca96e3f30068b9fb4a78b3c598bdc4c
maestus <khemzys@gmail.com>
2016-10-31 19:27:25 +0100

    Adding comment

--- Abstract_and_multiple_legacy/Diamant.cpp
+++ Abstract_and_multiple_legacy/Diamant.cpp
@@ -55,6 +55,6 @@ class Transmetter: public Printer, public Scanner{
 ///////////////////////////////////////////////////
 /**************************************************
 
-class Printer: virtual public Device{}
+class Printer: virtual public Device{} To have a id
 
 **************************************************/
