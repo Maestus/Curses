2ad0b1c9779d55e75bd9d1c01a90b768f07f393b
maestus <khemzys@gmail.com>
2016-10-31 19:25:29 +0100

    Ending Curse

---
+++ Abstract_and_multiple_legacy/Diamant.cpp
@@ -0,0 +1,60 @@
+/**************************************************
+
+Diamant --> Dans PrinterScanner, Scanner et Printer
+partagent le même id
+
+class Device{
+  int getId(){return id;}
+private:
+  int id;
+};
+
+class Printer: public Device{
+  void Print(){}
+};
+
+class Scanner: public Device{
+
+};
+
+class PrinterScanner: public Printer, public Scanner{
+  void Print(){}
+};
+
+**************************************************/
+
+
+/**************************************************
+
+By Default case ---> Diamant where in Transmetter :
+Receiver and Sender DOESN'T HAVE THE SAME id
+
+class Buffer{
+  int getId(){return id;}
+private:
+  int id;
+};
+
+class Receiver: public Buffer{
+  void Print(){}
+};
+
+class Sender: public Buffer{
+
+};
+
+class Transmetter: public Printer, public Scanner{
+  void Print(){}
+};
+
+**************************************************/
+
+
+///////////////////////////////////////////////////
+//HOW
+///////////////////////////////////////////////////
+/**************************************************
+
+class Printer: virtual public Device{}
+
+**************************************************/
--- Abstract_and_multiple_legacy/multiple_legacy.cpp
+++ Abstract_and_multiple_legacy/multiple_legacy.cpp
@@ -1,15 +1,71 @@
-/**********
-Simple plot
-**********/
+#include <iostream>
+using namespace std;
+
+/***********************************
+Non Virtual
 
 class B1{
-  int b1;
+public:
+  int b;
+  int get(){return b;}
 };
 
 class B2{
-  int b2;
+public:
+  int b;
+  int get(){return b;}
 };
 
 class D: public B1, public B2{
-
+  int get(){ return B1::get() + B2::get(); } // Masquage
 };
+
+int main(){
+  D d;
+
+  Due to same name in B1 and B2
+
+  d.B1::b = 1;
+  d.B2::b = 2;
+  cout << d.B1::get() << endl;
+  cout << d.B2::get();
+  return 0;
+}
+
+***********************************/
+
+/***********************************
+Virtual
+
+class B1{
+public:
+  int b;
+  virtual int get(){return b;}
+};
+
+class B2{
+public:
+  int b;
+  virtual int get(){return b;}
+};
+
+class D: public B1, public B2{
+  int get(){ return B1::get() + B2::get(); } // Redefinition
+};
+
+
+
+int main(){
+  D d;
+
+  Due to same name in B1 and B2
+
+  d.B1::b = 1;
+  d.B2::b = 2;
+  cout << d.B1::get() << endl;
+  cout << d.B2::get();
+  return 0;
+}
+
+
+***********************************/
